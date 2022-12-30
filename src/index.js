4f1c250e7f0cd2774f9c6bf68c8216b59bb34ae3
george <hyunrim03@naver.com>
2022-12-30 14:44:59 +0900

    라우터 추가

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import { store } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
