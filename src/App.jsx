aceec8d08240f7816b9317592b09f3af03bf00ac
george <hyunrim03@naver.com>
2023-01-16 14:46:51 +0900

    update_tokenStatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Post from './pages/post';
 import SignIn from './pages/signin';
 import SignUp from './pages/signup';
 
+const userData = 'userData';
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const token = localStorage.getItem(userData);
+    if (token) {
+      navigate('/home');
+    }
+  }, []);
+  navigate('/');
   return (
     <div className="App">
       <Routes>
