7926c04627bc1dc3b60c8212e4d947039cbc3866
george <hyunrim03@naver.com>
2023-01-22 17:44:03 +0900

    update_export-ListItem

--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useNavigate } from 'react-router-dom';
 
-const ListItem = (props) => {
+export const ListItem = (props) => {
   const navigate = useNavigate();
   const { pk, title, nickname, category, location } = props;
   return (
@@ -31,4 +31,45 @@ ListItem.propTypes = {
   nickname: PropTypes.string.isRequired,
 };
 
-export default ListItem;
+export const PostingTitleListItem = (props) => {
+  const { title } = props;
+  return (
+    <div className=" p-5 w-inputItemWidth h-postInputItemHeight border-subColor_grey border-border_md rounded-borderRadius_sm flex justify-start mb-4">
+      <span className="font-semibold text-fontSize_md text-fontColor_black">{title}</span>
+    </div>
+  );
+};
+PostingTitleListItem.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export const PostingBodyListItem = (props) => {
+  const { body } = props;
+  return (
+    <div className="p-5 w-inputItemWidth h-postInputItemContentHeight border-subColor_grey border-border_md rounded-borderRadius_sm flex flex-col items-start mb-4">
+      <span className="font-bold text-fontSize_lg text-fontColor_black">요청사항</span>
+      <p className="font-semibold text-fontSize_md text-fontColor_black mt-4">{body}</p>
+    </div>
+  );
+};
+PostingBodyListItem.propTypes = {
+  body: PropTypes.string.isRequired,
+};
+
+export const PostingCommentListItem = (props) => {
+  const { nickname, comment } = props;
+  return (
+    <div
+      id="inputItem"
+      className="py-9 px-8 w-inputItemWidth h-inputItemHeight border-subColor_grey border-border_md rounded-borderRadius_lg flex justify-start items-center focus mt-4"
+    >
+      <span className=" text-fontSize_md text-fontColor_Black font-semibold">{nickname}</span>
+      <span className=" text-fontSize_lg text-subColor_grey font-semibold ml-2"> : </span>
+      <span className="ml-2 w-auto">{comment}</span>
+    </div>
+  );
+};
+PostingCommentListItem.propTypes = {
+  nickname: PropTypes.string.isRequired,
+  comment: PropTypes.string.isRequired,
+};
