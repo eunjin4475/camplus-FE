1d612580739df1aae9d2349a49235d2dbad863d7
george <hyunrim03@naver.com>
2023-01-22 18:19:49 +0900

    update_commentData-state

--- src/components/contents/postingContent.jsx
+++ src/components/contents/postingContent.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getData } from '../../redux/listDataSlice';
 import { PostingBodyListItem, PostingCommentListItem, PostingTitleListItem } from '../listItem';
 import { PostInputItem } from '../inputItem';
 import { SubmitEventBtn2 } from '../button';
+import { postComment } from '../../services/user.service';
 
 const PostingContent = () => {
   const url = useParams();
   const dispatch = useDispatch();
+  const [commentData, setCommentData] = useState('');
   useEffect(() => {
     dispatch(getData(url.id));
-  }, []);
+  }, [postComment]);
   const listData = useSelector((state) => {
     return state.listData.data;
   });
@@ -27,15 +29,19 @@ const PostingContent = () => {
             return <PostingCommentListItem nickname={data.nickname} comment={data.comment} />;
           })}
       </div>
-      <form>
+      <form className="flex-row flex justify-between items-center">
         <PostInputItem
           className="py-9 px-8 w-inputItemWidth h-postInputItemHeight border-subColor_grey border-border_md rounded-borderRadius_lg flex justify-start items-center focus"
           itemType="댓글"
+          onChange={(event) => {
+            setCommentData(event.target.value);
+          }}
         />
         <SubmitEventBtn2
           text="보내기"
-          submitEvent={() => {
-            console.log(2);
+          submitEvent={(e) => {
+            e.preventDefault();
+            postComment(commentData);
           }}
         />
       </form>
