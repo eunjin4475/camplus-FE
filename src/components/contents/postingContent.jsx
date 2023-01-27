eca7959149d14341725f7e9b6d37900cc6f2a3e4
george <hyunrim03@naver.com>
2023-01-27 17:24:04 +0900

    add_listDeleteBtn

--- src/components/contents/postingContent.jsx
+++ src/components/contents/postingContent.jsx
@@ -1,29 +1,50 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getData } from '../../redux/listDataSlice';
 import { PostingBodyListItem, PostingCommentListItem, PostingTitleListItem } from '../listItem';
-import InputItem from '../inputItem';
+import { InputItem } from '../inputItem';
 import { MainBtn } from '../button';
-import { postComment } from '../../services/user.service';
+import { postComment, deleteList } from '../../services/user.service';
 
 const PostingContent = () => {
   const url = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
+  const userData = JSON.parse(localStorage.getItem('userData'));
   const [commentData, setCommentData] = useState({
     post: url.id,
     text: '',
   });
-  useEffect(() => {
-    dispatch(getData(url.id));
-  }, [postComment]);
   const singleListData = useSelector((state) => {
     return state.listData.data;
   });
+
+  useEffect(() => {
+    dispatch(getData(url.id));
+  }, [postComment]);
+
   return (
     <div className="flex flex-col justify-center items-center overflow-auto">
       <PostingTitleListItem title={singleListData.title} />
       <PostingBodyListItem body={singleListData.body} />
+      {singleListData.author && userData.nickname === singleListData.author.nickname ? (
+        <MainBtn
+          text="삭제"
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            const deleteResponse = deleteList(url.id);
+            deleteResponse
+              .then(() => {
+                navigate('/home');
+              })
+              .catch(() => {});
+          }}
+          buttonClassName=" w-categoryBtnWidth h-categoryBtnHeight bg-mainColor rounded-borderRadius_sm"
+          spanClassName="font-bold text-fontSize_sm text-textColor_white"
+        />
+      ) : null}
       <div className="h-commentHeight overflow-auto mb-4">
         {singleListData.comments &&
           singleListData.comments.map((singleListDataComment) => {
